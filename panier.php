f14425e5e242833c64027a464df56401044e9e1f
windev15a <abdellahwindev@gmail.com>
2022-02-12 11:35:29 +0100

    mini changement

--- panier.php
+++ panier.php
@@ -9,10 +9,10 @@ $reqPanier->bindValue(':status', false, PDO::PARAM_BOOL );
 $reqPanier->execute();
 $dataPanier = $reqPanier->fetchAll(PDO::FETCH_ASSOC);
 
-    echo '<pre style="margin-left:250px">';
-    print_r(unserialize($dataPanier[0]['produit'])['id_produit']);
-    // print_r($dataPanier);
-    echo '</pre>';
+    // echo '<pre style="margin-left:250px">';
+    // print_r(unserialize($dataPanier[0]['produit'])['id_produit']);
+    // // print_r($dataPanier);
+    // echo '</pre>';
 
 
 
@@ -24,7 +24,7 @@ require_once('inc/inc_front/nav.inc.php');
             <h1 class="text-center my-5">Votre panier</h1>
 
             <?php foreach($dataPanier as $key=>$value): ?>
-            <div class="container col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 mx-auto d-flex justify-content-center shadow-sm px-0">
+            <div class="container col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 mx-auto d-flex justify-content-center shadow-sm px-0 mb-2">
                 <div class="col-md-2 bg-white p-2">
                     <a href="fiche_produit.html"><img src="<?= unserialize($value['produit'])['photo'] ?>" alt="produit 1" class="img-panier"></a>
                 </div>
@@ -41,7 +41,7 @@ require_once('inc/inc_front/nav.inc.php');
             <?php endforeach; ?>
             
             <div class="container col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 d-flex justify-content-end align-items-center shadow-sm px-0 py-3 bg-white mt-2 mb-3">
-                <h5 class="m-0 px-2 fw-bold">Sous total (3 articles) : 170.99€</h5>
+                <h5 class="m-0 px-2 fw-bold">Sous total (<?= $reqPanier->rowCount() ?> articles) : 170.99€</h5>
             </div>
             <div class="container col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 p-0 text-end mb-5">
                 <a href="" class="btn btn-dark">FINALISER LA COMMANDE</a>
